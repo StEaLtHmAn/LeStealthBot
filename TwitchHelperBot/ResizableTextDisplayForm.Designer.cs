1ca2f200fcb33bb779debd1ba3e02a1220e058fd
Nicolas Tyler <email.stealthman@gmail.com>
2023-05-18 13:02:37 +0200

    Made the viewer list always on top and fixed the selection and scrolling

--- TwitchHelperBot/ResizableTextDisplayForm.Designer.cs
+++ TwitchHelperBot/ResizableTextDisplayForm.Designer.cs
@@ -32,23 +32,27 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox1 = new TwitchHelperBot.RichTextBoxEx();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.viewUserDetailsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.banUserToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // textBox2
             // 
             this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox2.Location = new System.Drawing.Point(40, 8);
+            this.textBox2.Location = new System.Drawing.Point(40, 3);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(199, 20);
+            this.textBox2.Size = new System.Drawing.Size(208, 20);
             this.textBox2.TabIndex = 2;
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 11);
+            this.label1.Location = new System.Drawing.Point(6, 6);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(32, 13);
             this.label1.TabIndex = 3;
@@ -66,21 +70,41 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.richTextBox1.Location = new System.Drawing.Point(3, 31);
+            this.richTextBox1.Location = new System.Drawing.Point(3, 27);
             this.richTextBox1.Margin = new System.Windows.Forms.Padding(0);
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.ReadOnly = true;
             this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
-            this.richTextBox1.Size = new System.Drawing.Size(239, 262);
+            this.richTextBox1.Size = new System.Drawing.Size(248, 271);
             this.richTextBox1.TabIndex = 4;
             this.richTextBox1.Text = "";
             this.richTextBox1.WordWrap = false;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.viewUserDetailsToolStripMenuItem,
+            this.banUserToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(164, 48);
+            // 
+            // viewUserDetailsToolStripMenuItem
+            // 
+            this.viewUserDetailsToolStripMenuItem.Name = "viewUserDetailsToolStripMenuItem";
+            this.viewUserDetailsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.viewUserDetailsToolStripMenuItem.Text = "View User Details";
+            // 
+            // banUserToolStripMenuItem
+            // 
+            this.banUserToolStripMenuItem.Name = "banUserToolStripMenuItem";
+            this.banUserToolStripMenuItem.Size = new System.Drawing.Size(163, 22);
+            this.banUserToolStripMenuItem.Text = "Ban User";
             // 
             // ResizableTextDisplayForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(245, 296);
+            this.ClientSize = new System.Drawing.Size(254, 301);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox2);
@@ -92,7 +116,9 @@
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Viewer List";
+            this.TopMost = true;
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ResizableTextDisplayForm_FormClosing);
+            this.contextMenuStrip1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -102,6 +128,9 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Timer timer1;
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private RichTextBoxEx richTextBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem viewUserDetailsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem banUserToolStripMenuItem;
     }
 }
--- TwitchHelperBot/ResizableTextDisplayForm.cs
+++ TwitchHelperBot/ResizableTextDisplayForm.cs
@@ -4,11 +4,14 @@ using RestSharp;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.UI.WebControls;
 using System.Windows.Forms;
 using System.Xml.Linq;
 using static System.Collections.Specialized.BitVector32;
@@ -35,59 +38,7 @@ namespace TwitchHelperBot
 
         private void textBox2_TextChanged(object sender, EventArgs e)
         {
-            richTextBox1.Clear();
-
-            TimeSpan SessionDuration = DateTime.UtcNow - sessionStart;
-            TimeSpan totalDuration = SessionDuration;
-            double SessionHoursWatched = WatchTimeDictionary.Sum(x => x.Value.TotalHours);
-            double totalHours = SessionHoursWatched;
-            double currentAverage = ViewerCountPerMinute.Count > 0 ? ViewerCountPerMinute.Average() : 0;
-            double TotalAverage = currentAverage;
-            int peakViewers = WatchTimeDictionary.Count;
-            foreach (SessionData session in Sessions)
-            {
-                totalDuration += session.DateTimeEnded - session.DateTimeStarted;
-                TotalAverage += session.AverageViewerCount;
-                totalHours += session.WatchTimeData.Sum(x => x.Value.TotalHours);
-                if(session.PeakViewerCount > peakViewers)
-                    peakViewers = session.PeakViewerCount;
-            }
-
-            AppendText(
-
-                $"Overall Stats:{Environment.NewLine}" +
-                $"- Session Count: {Sessions.Count}{Environment.NewLine}" +
-                $"- Duration: {totalDuration:hh':'mm':'ss}{Environment.NewLine}" +
-                $"- Average / Peak Viewers: {TotalAverage / (Sessions.Count+1):0.##} / {peakViewers}{Environment.NewLine}" +
-                $"- Hours Watched: {totalHours + WatchTimeDictionary.Sum(x => x.Value.TotalHours):0.##}{Environment.NewLine}"
-
-                , Color.Gold);
-            AppendText(
-
-                $"Session Stats:{Environment.NewLine}" +
-                $"- Duration: {SessionDuration:hh':'mm':'ss}{Environment.NewLine}" +
-                $"- Average / Peak Viewers: {currentAverage:0.##} / {WatchTimeDictionary.Count}{Environment.NewLine}" +
-                $"- Hours Watched: {SessionHoursWatched:0.##}{Environment.NewLine}" +
-                $"{Environment.NewLine}"
-
-                , Color.Green);
-
-            int count = 1;
-            foreach (KeyValuePair<string, TimeSpan> kvp in WatchTimeDictionary.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
-            {
-                if (kvp.Key.ToLower().Contains(textBox2.Text.Trim().ToLower()))
-                {
-                    if (ViewerNames.Contains(kvp.Key))
-                    {
-                        AppendText($"{count}. {kvp.Key} - ({kvp.Value:hh':'mm':'ss}){Environment.NewLine}", richTextBox1.ForeColor);
-                        count++;
-                    }
-                    else
-                    {
-                        AppendText($"{kvp.Key} - ({kvp.Value:hh':'mm':'ss}){Environment.NewLine}", Color.Red);
-                    }
-                }
-            }
+            UpdateText();
         }
 
         private async void timer1_Tick(object sender, EventArgs e)
@@ -104,7 +55,10 @@ namespace TwitchHelperBot
 
                     TimeSpan lastViewerCountSpan = DateTime.UtcNow - lastViewerCountCheck;
                     if (lastViewerCountSpan.TotalMinutes >= 1 || ViewerCountPerMinute.Count == 0)
+                    {
                         ViewerCountPerMinute.Add(Viewers.Count);
+                        lastViewerCountCheck = DateTime.UtcNow;
+                    }
                     TimeSpan span = DateTime.UtcNow - lastCheck;
                     lastCheck = DateTime.UtcNow;
                     foreach (string name in ViewerNames)
@@ -122,81 +76,72 @@ namespace TwitchHelperBot
                     {
                         try
                         {
-                            richTextBox1.Clear();
-
-                            TimeSpan SessionDuration = DateTime.UtcNow - sessionStart;
-                            TimeSpan totalDuration = SessionDuration;
-                            double SessionHoursWatched = WatchTimeDictionary.Sum(x => x.Value.TotalHours);
-                            double totalHours = SessionHoursWatched;
-                            double currentAverage = ViewerCountPerMinute.Count > 0 ? ViewerCountPerMinute.Average() : 0;
-                            double TotalAverage = currentAverage;
-                            int peakViewers = WatchTimeDictionary.Count;
-                            foreach (SessionData session in Sessions)
-                            {
-                                totalDuration += session.DateTimeEnded - session.DateTimeStarted;
-                                TotalAverage += session.AverageViewerCount;
-                                totalHours += session.WatchTimeData.Sum(x => x.Value.TotalHours);
-                                if (session.PeakViewerCount > peakViewers)
-                                    peakViewers = session.PeakViewerCount;
-                            }
-
-                            AppendText(
-
-                                $"Overall Stats:{Environment.NewLine}" +
-                                $"- Session Count: {Sessions.Count}{Environment.NewLine}" +
-                                $"- Duration: {totalDuration:hh':'mm':'ss}{Environment.NewLine}" +
-                                $"- Average / Peak Viewers: {TotalAverage / (Sessions.Count + 1):0.##} / {peakViewers}{Environment.NewLine}" +
-                                $"- Hours Watched: {totalHours + WatchTimeDictionary.Sum(x => x.Value.TotalHours):0.##}{Environment.NewLine}"
-
-                                , Color.Gold);
-                            AppendText(
-
-                                $"Session Stats:{Environment.NewLine}" +
-                                $"- Duration: {SessionDuration:hh':'mm':'ss}{Environment.NewLine}" +
-                                $"- Average / Peak Viewers: {currentAverage:0.##} / {WatchTimeDictionary.Count}{Environment.NewLine}" +
-                                $"- Hours Watched: {SessionHoursWatched:0.##}{Environment.NewLine}" +
-                                $"{Environment.NewLine}"
-
-                                , Color.Green);
+                            UpdateText();
                         }
                         catch { }
                     }));
-                    int count = 1;
-                    foreach (KeyValuePair<string, TimeSpan> kvp in WatchTimeDictionary.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
-                    {
-                        if (kvp.Key.ToLower().Contains(textBox2.Text.Trim().ToLower()))
-                        {
-                            Invoke(new Action(() =>
-                            {
-                                try
-                                {
-                                    if (ViewerNames.Contains(kvp.Key))
-                                    {
-                                        AppendText($"{count}. {kvp.Key} - ({kvp.Value:hh':'mm':'ss}){Environment.NewLine}", richTextBox1.ForeColor);
-                                        count++;
-                                    }
-                                    else
-                                    {
-                                        AppendText($"{kvp.Key} - ({kvp.Value:hh':'mm':'ss}){Environment.NewLine}", Color.Red);
-                                    }
-                                }
-                                catch { }
-                            }));
-                        }
-                    }
                 }
                 catch { }
             });
             timer1.Enabled = true;
         }
 
-        public void AppendText(string text, Color color)
+        private void UpdateText()
         {
-            richTextBox1.SuspendLayout();
-            richTextBox1.SelectionColor = color;
-            richTextBox1.AppendText(text);
-            richTextBox1.ScrollToCaret();
-            richTextBox1.ResumeLayout();
+            TimeSpan SessionDuration = DateTime.UtcNow - sessionStart;
+            TimeSpan totalDuration = SessionDuration;
+            double SessionHoursWatched = WatchTimeDictionary.Sum(x => x.Value.TotalHours);
+            double totalHours = SessionHoursWatched;
+            double currentAverage = ViewerCountPerMinute.Count > 0 ? ViewerCountPerMinute.Average() : 0;
+            double TotalAverage = currentAverage;
+            int peakViewers = WatchTimeDictionary.Count;
+            foreach (SessionData session in Sessions)
+            {
+                totalDuration += session.DateTimeEnded - session.DateTimeStarted;
+                TotalAverage += session.AverageViewerCount;
+                totalHours += session.WatchTimeData.Sum(x => x.Value.TotalHours);
+                if (session.PeakViewerCount > peakViewers)
+                    peakViewers = session.PeakViewerCount;
+            }
+
+            richTextBox1.SuspendPainting();
+            richTextBox1.Clear();
+            richTextBox1.SelectionColor = Color.Gold;
+            richTextBox1.AppendText(
+                $"Overall Stats:{Environment.NewLine}" +
+                $"- Session Count: {Sessions.Count}{Environment.NewLine}" +
+                $"- Duration: {totalDuration:hh':'mm':'ss}{Environment.NewLine}" +
+                $"- Average/Peak Viewers: {TotalAverage / (Sessions.Count + 1):0.##} / {peakViewers}{Environment.NewLine}" +
+                $"- Hours Watched: {totalHours + WatchTimeDictionary.Sum(x => x.Value.TotalHours):0.##}{Environment.NewLine}"
+                );
+            richTextBox1.SelectionColor = Color.Green;
+            richTextBox1.AppendText(
+                $"Session Stats:{Environment.NewLine}" +
+                $"- Duration: {SessionDuration:hh':'mm':'ss}{Environment.NewLine}" +
+                $"- Current/Average/Peak Viewers: {ViewerNames.Length} / {currentAverage:0.##} / {WatchTimeDictionary.Count}{Environment.NewLine}" +
+                $"- Hours Watched: {SessionHoursWatched:0.##}{Environment.NewLine}" +
+                $"{Environment.NewLine}"
+                );
+
+            int count = 1;
+            foreach (KeyValuePair<string, TimeSpan> kvp in WatchTimeDictionary.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+            {
+                if (kvp.Key.ToLower().Contains(textBox2.Text.Trim().ToLower()))
+                {
+                    if (ViewerNames.Contains(kvp.Key))
+                    {
+                        richTextBox1.SelectionColor = richTextBox1.ForeColor;
+                        richTextBox1.AppendText($"{count}. {kvp.Key} - ({kvp.Value:hh':'mm':'ss}){Environment.NewLine}");
+                        count++;
+                    }
+                    else
+                    {
+                        richTextBox1.SelectionColor = Color.Red;
+                        richTextBox1.AppendText($"{kvp.Key} - ({kvp.Value:hh':'mm':'ss}){Environment.NewLine}");
+                    }
+                }
+            }
+            richTextBox1.ResumePainting();
         }
 
         public JArray GetChattersList()
@@ -207,8 +152,8 @@ namespace TwitchHelperBot
             client.AddDefaultHeader("Client-ID", Globals.clientId);
             client.AddDefaultHeader("Authorization", "Bearer " + Globals.access_token);
             RestRequest request = new RestRequest("https://api.twitch.tv/helix/chat/chatters", Method.Get);
-            //request.AddQueryParameter("broadcaster_id", "526375465");
-            request.AddQueryParameter("broadcaster_id", Globals.userDetailsResponse["data"][0]["id"].ToString());
+            request.AddQueryParameter("broadcaster_id", "526375465");
+            //request.AddQueryParameter("broadcaster_id", Globals.userDetailsResponse["data"][0]["id"].ToString());
             request.AddQueryParameter("moderator_id", Globals.userDetailsResponse["data"][0]["id"].ToString());
             request.AddQueryParameter("first", 1000);
             RestResponse response = client.Execute(request);
@@ -278,6 +223,20 @@ namespace TwitchHelperBot
             public int PeakViewerCount { get; set; }
             public double CombinedHoursWatched { get; set; }
             public Dictionary<string, TimeSpan> WatchTimeData { get; set; }
+        }
+
+        private void richTextBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                int wordIndex = richTextBox1.Text.Substring(0, richTextBox1.GetCharIndexFromPosition(e.Location)).LastIndexOfAny(new char[] { ' ', '\r', '\n' }) + 1;
+                string word = richTextBox1.Text.Substring(wordIndex, richTextBox1.Text.IndexOfAny(new char[] { ' ', '\r', '\n' }, wordIndex) - wordIndex);
+
+                if (WatchTimeDictionary.ContainsKey(word))
+                {
+                    contextMenuStrip1.Show(richTextBox1, e.Location);
+                }
+            }
         }
     }
 }
